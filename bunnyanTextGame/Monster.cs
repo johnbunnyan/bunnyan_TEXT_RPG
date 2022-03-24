e94caf20cbb0cf33ba316982a90ff2758c125104
jeongsang-gyu <jsk717543@gmail.com>
2022-03-25 01:48:11 +0900

    기본 세

---
+++ bunnyanTextGame/Monster.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bunnyanTextGame
+{
+    public enum MonsterType
+    {
+        None = 0,
+        Slime = 1,
+        Orc = 2,
+        Skeleton = 3
+    };
+
+    public class Monster : Creature
+    {
+        protected MonsterType Type;
+        protected Monster(MonsterType type) : base(CreatureType.Monster)
+        {
+            this.Type = type;
+        }
+       public MonsterType GetMonsterType() { return Type; }
+    }
+
+    class Slime : Monster
+    {
+        public Slime() : base(MonsterType.Slime)
+        {
+            SetInfo(10, 10);
+        }
+    }
+
+    class Orc : Monster
+    {
+        public Orc() : base(MonsterType.Orc)
+        {
+            SetInfo(20, 15);
+        }
+    }
+
+    class Skeleton : Monster
+    {
+        public Skeleton() : base(MonsterType.Skeleton)
+        {
+            SetInfo(15, 25);
+        }
+    }
+}
---
+++ bunnyanTextGame/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bunnyanTextGame
+{
+    public enum PlyaerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+
+    public class Player : Creature
+    {
+        protected PlyaerType type = PlyaerType.None;
+
+        protected Player(PlyaerType type) : base(CreatureType.Player)
+        {
+            this.type = type;
+        }
+
+        public PlyaerType GetPlyaerType() { return type; }
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlyaerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlyaerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlyaerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+
+}
